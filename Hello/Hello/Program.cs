7168afa7d6300f3b4ebf060b6cb50d4fa5f31fd8
RubenA2109 <a14685@oficina.pt>
2026-04-15 12:05:27 +0100

    Add files via upload

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Hello.ws;
+
+internal class Program
+{
+
+    static void testaNomes(string child1, string child2 = "", string child3 = "")
+    {
+        if (child2 == "")
+        {
+            Console.WriteLine(child1);
+        }
+        else
+        {
+            if (child3 == "")
+            {
+                Console.WriteLine(child2);
+            }
+            else
+            {
+                Console.WriteLine(child3);
+            }
+        }
+    }
+
+    static int MyMethod(int num)
+    {
+        return num + 1;
+    }
+
+    static void Main(string[] args)
+    {
+        Carro newCar = new Carro();
+        newCar.marca = "Oopel";
+        newCar.modelo = "Corsa";
+        newCar.matricula = "00-aa-00";
+        newCar.ano = 2004;
+        newCar.mostraDados();
+    }
+
+
+    //int myNum = 5;
+    //double myDoubleNum = 5.99D;
+    //float myFloatNum = 1.123456789f;
+    //char myLetter = 'D';
+    //bool MyBool = true;
+    //string myText = "Hello";
+
+    //Console.Write(myNum);
+    //Console.Write(myDoubleNum);
+
+}
+
+
---
+++ Hello/Hello/ws/Carro.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello.ws
+{
+    internal class Carro
+    {
+        public string marca;
+        public string modelo;
+        public string matricula;
+        public int ano;
+
+        public void setMarca(string marca)
+        {
+            this.marca = marca;
+        }
+
+        public string getMarca()
+        {
+            return marca;
+        }
+
+        public void setModelo(string modelo)
+        {
+            this.modelo = modelo;
+        }
+
+        public string getModelo()
+        {
+            return modelo;
+        }
+        public void setMatricula(string matricula)
+        {
+            this.matricula = matricula;
+        }
+
+        public string getMatricula()
+        {
+            return matricula;
+        }
+        public void setAno(int ano)
+        {
+            this.ano = ano;
+        }
+
+        public int getAno()
+        {
+            return ano;
+        }
+
+        public Carro(string marca, string modelo, string matricula, int ano)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.matricula = matricula;
+            this.ano = ano;
+        }
+
+        public Carro()
+        {
+            this.marca = "";
+            this.modelo = "";
+            this.matricula = "";
+            this.ano = 0;
+
+        }
+        public void mostraDados()
+        {
+            Console.WriteLine(marca);
+            Console.WriteLine(modelo);
+            Console.WriteLine(matricula);
+            Console.WriteLine(ano);
+
+        }
+    }
+}
